5123fa32b3676b1ab3acd61309e19ac57c4fb369
kanamycine <jinyeong5320@gmail.com>
2021-11-24 00:23:39 +0900

    1~10

---
+++ src/Q1009.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Q1009 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt());
+    }
+}
---
+++ src/Q1010.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Q1010 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.next());
+    }
+}
