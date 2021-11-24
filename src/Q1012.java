18df1f4ff6ca16957b56971a335e760b9c0ddb71
kanamycine <jinyeong5320@gmail.com>
2021-11-24 15:15:27 +0900

    11~12

---
+++ src/Q1012.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Q1012 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("%.6f", scanner.nextFloat());
+    }
+}
