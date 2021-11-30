6d72bf1e3fe778f25b199e60dac553564924a176
kanamycine <jinyeong5320@gmail.com>
2021-11-30 23:28:41 +0900

    Q1034

---
+++ src/Q1034.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Q1034 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		String b = Integer.toOctalString(a);
+		System.out.println(b);
+	}
+}
