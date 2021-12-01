b91e3f8ad331556ee3a216c8697ab7a0dce3a0a7
kanamycine <jinyeong5320@gmail.com>
2021-12-01 14:16:10 +0900

    Q1035 ~ Q1040

---
+++ src/Q1036.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Q1036 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		char b = a.charAt(0);
+		System.out.println(b - '0');
+	}
+}
---
+++ src/Q1037.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Q1037 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int b = sc.nextInt();
+		System.out.println((char)b);
+	}
+}
